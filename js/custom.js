71b216c32c5d0b00f637dd3faae93d5cfb14cb33
Phuong Marangoni <61428872+phuong-marangoni@users.noreply.github.com>
2020-02-29 15:07:32 -0500

    Initial commit of personal page files
    Personal portfolio

---
+++ js/custom.js
@@ -0,0 +1,6 @@
+let nav = document.getElementById('js-nav-menu');
+let navToggle = document.getElementById('js-nav-toggle');
+
+navToggle.addEventListener('click', function () {
+  nav.classList.toggle('active');
+});
