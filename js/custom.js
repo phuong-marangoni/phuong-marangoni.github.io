486f61f3a7b448017a238c5843e3820afce170fd
Phuong <ngopt.0@gmail.com>
2020-05-08 15:45:44 -0400

    Updated descriptions and renamed files.

--- js/custom.js
+++
@@ -1,6 +0,0 @@
-let nav = document.getElementById('js-nav-menu');
-let navToggle = document.getElementById('js-nav-toggle');
-
-navToggle.addEventListener('click', function () {
-  nav.classList.toggle('active');
-});
